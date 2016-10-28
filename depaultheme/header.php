b1861ad0b9999064547dcdaed2012f1e9642f4a7
Christine McClure <camcclure@gmail.com>
2016-10-28 11:24:53 -0500

    update header.php file to include GA tracking code (already in production)

--- depaultheme/header.php
+++ depaultheme/header.php
@@ -18,6 +18,35 @@
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 <?php wp_head(); ?>
+
+<script type="text/javascript">
+//Google Analytics
+//Set tracking to base report
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-5169126-1']);
+//Set Custom Variable to Type-Wordpress
+_gaq.push(['_setCustomVar',
+      1,                   // This custom var is set to slot #1.  Required parameter.
+      'platform',           // The top-level name for our report.  Required parameter.
+      'wordpress',  // Sets the value of "Platform".  Required parameter.
+      3                    // Sets the scope to page-level.  Optional parameter.
+   ]);
+
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+try{
+var pageTracker = _gat._getTracker("UA-5169126-1");
+pageTracker._setAllowLinker(true);
+pageTracker._trackPageview();
+} catch(err) {}
+</script>
+
 </head>
 
 <body <?php body_class(); ?>>
