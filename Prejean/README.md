9f682fc765b773ef0ecb0d9132525ce3da587fd1
DPU\gpangani <gpangani@depaul.edu>
2017-07-07 14:38:41 -0500

    Sister Helen Prejean website files

---
+++ Prejean/README.md
@@ -0,0 +1,8 @@
+# Matata Child Theme
+This is a child theme of Matata http://justhemes.com/matata for Sister Helen Prejean website https://spca.depaul.press/prejean
+
+
+## Wordpress Plugins used:
+1. amr shortcode any widget
+2. PDF Light Viewer Plugin
+3. WP Meta and Date Remover
---
+++ Prejean/header.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Matata
+ */
+
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'matata' ); ?></a>
+
+	<header id="masthead" class="site-header" role="banner">
+		<div class="top-bar clear">
+
+			<a href="http://depaul.edu">
+				<img class="logo-short pull-left" src="https://spca.depaul.press/prejean/wp-content/uploads/sites/2/2017/03/depaul-tree-logo-white.png" />
+			</a>
+			<a class="depaul-branding" href="http://library.depaul.edu">
+				<span class="depaul-branding">University Library</span></a>
+				
+		<!--	<div class="top-menu">
+				<?php wp_nav_menu( array( 'theme_location' => 'secondary')); ?>
+			</div> 
+		-->
+
+
+		</div>
+
+           <p class="top-websites"><!-- <a class="topbrand" href="https://spca.depaul.press">Engage!</a> --> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://library.depaul.edu/special-collections/Pages/default.aspx">Special Collections Home</a></p>
+
+
+		<div class="site-branding">
+
+			<?php if ( get_header_image() ) : ?>
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+				<img src="<?php header_image(); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="">
+			</a>
+			<?php endif; // End header image check. ?>
+
+			<?php matata_the_custom_logo(); ?>
+
+			<?php
+			if ( is_front_page() && is_home() ) : ?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<?php else : ?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+			<?php
+			endif;
+
+			$description = get_bloginfo( 'description', 'display' );
+			if ( $description || is_customize_preview() ) : ?>
+				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+			<?php
+			endif; ?>
+		</div><!-- .site-branding -->
+
+		<nav id="site-navigation" class="main-navigation" role="navigation">
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><i class="fa fa-list-ul fa-2x"></i></button>
+			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+		</nav><!-- #site-navigation -->
+	</header><!-- #masthead -->
+
+	<div id="content" class="site-content">
