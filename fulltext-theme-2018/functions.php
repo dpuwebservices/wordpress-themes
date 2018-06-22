fe39a1350dc591dc2b0eaf6c4e5276b448e07299
DPU/gpangani <gpangani@depaul.edu>
2018-06-22 16:38:22 -0500

    Re-uploaded the correct file.
    The previously uploaded file was the incorrect version.

--- fulltext-theme-2018/functions.php
+++ fulltext-theme-2018/functions.php
@@ -4,7 +4,49 @@ if ( !defined( 'ABSPATH' ) ) exit;
 
 // BEGIN ENQUEUE PARENT ACTION
 // AUTO GENERATED - Do not modify or remove comment markers above or below:
-         
+       
+// Added the two lines below to customize the header and footer
+require get_stylesheet_directory() . '/inc/hooks/header.php';
+require get_stylesheet_directory() . '/inc/hooks/footer.php';
+
+// Line 16 to 48 was copied from /inc/template-tags.php file  
+// This is to force Wordpress to display "Display name publicly as" of the author instead of the username 
+// On Line 42, 'nickname' was changed to 'display_name'
+
+if ( ! function_exists( 'newslite_posted_on' ) ) :
+/**
+ * Prints HTML with meta information for the current post-date/time and author.
+ */
+function newslite_posted_on() {
+	global $post;
+	$author_id=$post->post_author;
+	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+	}
+
+	$time_string = sprintf( $time_string,
+		esc_attr( get_the_date( 'c' ) ),
+		esc_html( get_the_date() ),
+		esc_attr( get_the_modified_date( 'c' ) ),
+		esc_html( get_the_modified_date() )
+	);
+
+	$posted_on = sprintf(
+		esc_html_x( 'On %s', 'post date', 'newslite' ),
+		'<a href="' . esc_url(get_day_link(get_the_time('Y'), get_the_time('m'), get_the_time('d') )) . '" rel="bookmark">' . $time_string . '</a>'
+	);
+
+	$byline = sprintf(
+		esc_html_x( 'By %s', 'post author', 'newslite' ),
+		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( $author_id ) ) . '">' . esc_html( get_the_author_meta( 'display_name', $author_id ) ) . '</a></span>'
+	);
+
+	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
+
+}
+endif;
+
 if ( !function_exists( 'child_theme_configurator_css' ) ):
     function child_theme_configurator_css() {
         wp_enqueue_style( 'chld_thm_cfg_separate', trailingslashit( get_stylesheet_directory_uri() ) . 'ctc-style.css', array( 'slick-css','newslite-style' ) );
